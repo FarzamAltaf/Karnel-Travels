e9b5b43d42d8d84ccda2bc2b9fd61bdd79341e07
FarzamAltaf <farzamaltaf111@gmail.com>
2025-01-09 03:14:59 +0500

    signup-login modal setup

--- Models/connection.cs
+++ Models/connection.cs
@@ -8,7 +8,7 @@ namespace kernel.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=DESKTOP\\SQLEXPRESS;Database=Karnel-Travel;Integrated Security=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-GQ8EFUN\\SQLEXPRESS;Database=Karnel-Travel;Integrated Security=True;TrustServerCertificate=True;");
         }
     }
 }
--- obj/Debug/net8.0/kernel.AssemblyInfo.cs
+++ obj/Debug/net8.0/kernel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kernel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e18e6763c97e7c9fb4d1be706cec33c0fda5ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("kernel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kernel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
